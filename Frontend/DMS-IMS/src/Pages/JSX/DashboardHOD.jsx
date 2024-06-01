4b735597db3b0d50792b0a858e0781ac46aed02d
Krishna <kdwivedi343@gmail.com>
2024-06-01 10:34:30 +0530

    Made dashboard responsive and added react-router-dom

--- Frontend/DMS-IMS/src/Pages/JSX/DashboardHOD.jsx
+++ Frontend/DMS-IMS/src/Pages/JSX/DashboardHOD.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import '../CSS/DashboardHod.css'; // Import custom CSS
 import MainDashboard from "../../Components/MainDashboard";
@@ -11,6 +11,16 @@ import toast from "react-hot-toast";
 const DashboardHOD = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Collapse the sidebar when the location changes
+    const sidebar = document.getElementById("sidebarMenu");
+    if (sidebar) {
+      const bsCollapse = new window.bootstrap.Collapse(sidebar, { toggle: false });
+      bsCollapse.hide();
+    }
+  }, [location]);
+
   const handleClick = () => {
     localStorage.setItem("authToken", "");
     localStorage.setItem("role", "");
@@ -26,6 +36,9 @@ const DashboardHOD = () => {
           <a className="navbar-brand" href="#home">
             HOD Dashboard
           </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="ml-auto">
             <button className="btn btn-outline-light" onClick={handleClick}>
               Log out
@@ -37,7 +50,7 @@ const DashboardHOD = () => {
       <div className="container-fluid">
         <div className="row">
           {/* Sidebar */}
-          <nav className="col-md-3 col-lg-2 d-md-block bg-dark sidebar">
+          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
             <div className="position-sticky">
               <ul className="nav flex-column">
                 <li className="nav-item">
@@ -69,12 +82,13 @@ const DashboardHOD = () => {
             </div>
           </nav>
 
-          {location.pathname === '/dashboardhod' && <MainDashboard />}
-          {location.pathname === '/dashboardhod/add-subjects' && <AddSubject />}
-          {location.pathname === '/dashboardhod/add-students' && <AddStudent />}
-          {location.pathname === '/dashboardhod/add-faculty' && <AddFaculty />}
-          {location.pathname === '/dashboardhod/assign-faculty' && <AssignFaculty />}
-
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-content">
+            {location.pathname === '/dashboardhod' && <MainDashboard />}
+            {location.pathname === '/dashboardhod/add-subjects' && <AddSubject />}
+            {location.pathname === '/dashboardhod/add-students' && <AddStudent />}
+            {location.pathname === '/dashboardhod/add-faculty' && <AddFaculty />}
+            {location.pathname === '/dashboardhod/assign-faculty' && <AssignFaculty />}
+          </main>
         </div>
       </div>
     </div>
